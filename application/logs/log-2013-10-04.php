07246c4beaaea58335f036b4470602fca13f0829
wzw <wei.chungwei@gmail.com>
2013-11-15 14:22:47 +0800

    init master

---
+++ application/logs/log-2013-10-04.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-10-04 07:53:04 --> /home/wzw/www/citest/application/views/pages/home.php
+DEBUG - 2013-10-04 07:54:51 --> /home/wzw/www/citest/application/views/pages/home.php
+DEBUG - 2013-10-04 07:55:38 --> /home/wzw/www/citest/application/views/pages/home.php
+DEBUG - 2013-10-04 22:56:30 --> /home/wzw/www/citest/application/views/pages/home.php
+DEBUG - 2013-10-04 22:57:26 --> /home/wzw/www/citest/application/views/pages/home.php
+DEBUG - 2013-10-04 22:57:30 --> /home/wzw/www/citest/application/views/pages/home.php
+DEBUG - 2013-10-04 22:57:32 --> /home/wzw/www/citest/application/views/pages/home.php
